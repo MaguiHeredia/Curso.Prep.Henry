aef552d055d5c5c865da052128168dd8cc68635a
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-03-25 20:32:39 -0300

    tareaJV.Iterminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -218,10 +218,9 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var saludo = "Hola";
-  var espacio = " ";
+  var saludo = "Hola ";
   var signo = "!";
-  var saludo2 = saludo + espacio + nombre + signo;
+  var saludo2 = saludo + nombre + signo;
   return saludo2;
   
 }
@@ -229,13 +228,14 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return lado * 4;
   
 }
 
@@ -243,7 +243,8 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+var cuadrado = base * altura;
+return cuadrado / 2;
 }
 
 
@@ -251,7 +252,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro * 1.20;
 }
 
 
@@ -261,6 +262,20 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+ var a = "a";
+ var e = "e";
+ var i = "i";
+ var o = "o";
+ var u = "u";
+ var cantidad = 1;
+ if (letra == a || letra == e || letra == i || letra == o || letra == u) {
+  var frase = "Es vocal";
+  return frase;
+ }
+ else {
+   var frase2 = "Dato incorrecto";
+   return frase2;
+ }
   
 }
 
